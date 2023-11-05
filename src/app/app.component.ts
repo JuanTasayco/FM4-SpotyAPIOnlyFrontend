400584b6f202dd26229b800cc8623b0596e89988
JuanTasayco <juantasayco266@gmail.com>
2023-11-05 04:59:26 -0500

    fixSidebarAndInstallLenis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import Lenis from '@studio-freight/lenis';
+import { ScrollTrigger } from 'gsap/all';
+import { gsap } from 'gsap';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'FM3-SpotyApp';
+  ngOnInit(): void {
+    const lenis = new Lenis();
+
+    lenis.on('scroll', ScrollTrigger.update);
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+
+    gsap.ticker.lagSmoothing(0);
+  }
 }
